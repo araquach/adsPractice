4c59ff8970ef1c4e1b52580b400b336688e1ac54
Adam Carter <adamcarter@jakatasalon.co.uk>
2019-05-14 15:34:06 +0100

    Added Hello World

--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import "fmt"
 
-func speak(s string) {
-	fmt.Println("Hello", s, "- Have a nice day")
+type person struct {
+	Fname string
+	Lname string
+}
+
+func speak(f string, l string) {
+	fmt.Println("Hello", f, l, "- Have a nice day")
 }
 
 func main() {
-	speak("Adam Carter")
+	p1 := person{
+		"Adam",
+		"Carter",
+	}
+
+	speak(p1.Fname, p1.Lname)
 }
